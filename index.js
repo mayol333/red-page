bff8ae7dfbe51f52a68e59d44507f3e122a2bf63
mayol333 <s.moniczewski@gmail.com>
2023-06-21 18:06:39 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const hamburger = document.querySelector(".hamburger-icon");
+const mobileMenu = document.querySelector(".mobile-menu");
+const mobileMenuArrow = document.querySelector(".mobile-menu-arrow");
+const body = document.querySelector(".body");
+
+hamburger.addEventListener("click", () => {
+  mobileMenu.classList.add("show-mobile-menu");
+  body.classList.add("no-scroll");
+});
+mobileMenuArrow.addEventListener("click", () => {
+  mobileMenu.classList.remove("show-mobile-menu");
+  body.classList.remove("no-scroll");
+});
